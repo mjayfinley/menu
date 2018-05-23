d9a13ddce16d372dfa00c8a90fd4355ac895d776
Micah <mjayfinley@gmail.com>
2018-05-23 17:34:21 -0500

    Consolidated files, restructured filter function

---
+++ filter.js
@@ -0,0 +1,79 @@
+let listItems = document.getElementById("listItems")
+let menu = ''
+
+
+
+let fullMenuButton = document.getElementById("fullMenuButton")
+let startersMenuButton = document.getElementById("startersMenuButton")
+let entreesMenuButton= document.getElementById("entreesMenuButton")
+let dessertsMenuButton = document.getElementById("dessertsMenuButton")
+
+
+function fullMenu() {
+  listItems.innerHTML = ''
+  dishes.forEach(function(dish) {
+    let liItem = `<li>
+                    <img src="${dish.imageURL}">
+                    <label>${dish.title}</label>
+                    <p>${dish.description}</p>
+                    <p>$${dish.price}</p>
+                  </li>`
+
+    listItems.innerHTML += liItem
+  })
+}
+
+
+function filterMenu(menu) {
+  listItems.innerHTML = ''
+  dishes.filter(function(dish) {
+    if (dish.course == menu) {
+      let liItem = `<li>
+                      <img src="${dish.imageURL}">
+                      <label>${dish.title}</label>
+                      <p>${dish.description}</p>
+                      <p>$${dish.price}</p>
+                    </li>`
+
+      listItems.innerHTML += liItem
+    }
+  })
+}
+
+fullMenu()
+
+fullMenuButton.addEventListener('click',function(){
+  fullMenu()
+})
+
+startersMenuButton.addEventListener('click',function(){
+  filterMenu("Starters")
+})
+
+entreesMenuButton.addEventListener('click',function(){
+  filterMenu("Entrees")
+})
+
+dessertsMenuButton.addEventListener('click',function(){
+  filterMenu("Desserts")
+})
+
+/*
+function filterMenu(menu) {
+  listItems.innerHTML = ''
+  dishes.forEach(function(dish) {
+    if (menu != '') {
+      if (dish.course == menu) {
+        let liItem = `<li>
+                        <img src="${name.imageURL}">
+                        <label>${name.title}</label>
+                        <p>${name.description}</p>
+                        <p>$${name.price}</p>
+                      </li>`
+
+        listItems.innerHTML += liItem
+      }
+    }
+  })
+}
+*/
